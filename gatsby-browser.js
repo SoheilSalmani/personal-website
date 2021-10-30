645db01e9fa1fbcb012055de68ce2c6c607a1f27
Soheil Salmani <salmani.soheil@gmail.com>
2021-10-30 03:16:13 +0000

    ✨ feat: use `CodeBlock` components for code blocks in MDX content
    Configure the MDX provider to use the `CodeBlock` component for code blocks in
    MDX content.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,11 @@
 import { MDXProvider } from "@mdx-js/react"
 import React from "react"
 
+import CodeBlock from "./src/components/code-block"
 import Solution from "./src/components/solution"
 
 const components = {
+  code: props => <CodeBlock {...props} />,
   Solution: props => <Solution {...props} />,
 }
 
